5d317880758942a39a89aec1a36a8f04b3c16238
Nazli <nazlibulbuloglu@outlook.com>
2023-05-02 02:30:31 +0300

    tamamlandi

--- src/index.js
+++ src/index.js
@@ -7,3 +7,57 @@ import './less/index.less'
 
 
 // Kodlar buraya gelecek!
+document.addEventListener("keydown", function(event) {
+    document.body.style.backgroundColor = "blue";
+  });
+  
+  const myImage = document.querySelector(".img-content");
+
+myImage.addEventListener("wheel", function(event) {
+  event.preventDefault();
+  const currentWidth = this.clientWidth;
+  const currentHeight = this.clientHeight;
+  this.style.width = currentWidth + 50 + "px";
+  this.style.height = currentHeight + 50 + "px";
+});
+
+window.addEventListener("load", function() {
+    alert("Sayfa yüklendi!");
+  });
+  
+  const myInput = document.querySelector(".content-section");
+
+myInput.addEventListener("focus", function() {
+  this.style.border = "2px solid green";
+});
+
+myInput.addEventListener("blur", function() {
+  this.style.border = "1px solid black";
+});
+
+window.addEventListener("resize", function() {
+    alert("Sayfa boyutu değişti!");
+  });
+    
+  const navLinks = document.querySelectorAll('.nav-link');
+
+  navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      document.body.style.backgroundColor = 'pink';
+    });
+  });
+
+  const logo = document.querySelector('.logo-heading');
+
+logo.addEventListener('mouseover', () => {
+  logo.style.fontFamily = 'cursive';
+});
+
+const nav = document.querySelector('.nav');
+
+nav.addEventListener('click', () => {
+  document.body.style.transition = 'background-color 1s ease-in-out';
+  document.body.style.backgroundColor = 'lightblue';
+});
+
+
